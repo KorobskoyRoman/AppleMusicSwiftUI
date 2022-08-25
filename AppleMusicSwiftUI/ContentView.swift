7410987d0a194aaf409ff68f79ddb72c1355c0fe
KorobskoyRoman <shopone023@gmail.com>
2022-08-26 02:01:58 +0300

    Added tabView + navigation

--- AppleMusicSwiftUI/ContentView.swift
+++ AppleMusicSwiftUI/ContentView.swift
@@ -8,9 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var isSelected = true
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            TabView {
+                LibraryView()
+                    .tabItem {
+                        Image(systemName: "rectangle.stack.badge.play.fill")
+                        Text("Медиатека")
+                    }
+                Text("Радио")
+                    .tabItem {
+                        Image(systemName: "dot.radiowaves.left.and.right")
+                        Text("Радио")
+                    }
+                Text("Поиск")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Поиск")
+                    }
+            }
+            .accentColor(.red)
+            MiniPlayerView()
+                .padding(.top, 650)
+        }
+    }
+}
+
+struct LibraryView: View {
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center) {
+                Text("Ищите свою музыку?")
+                    .bold()
+                    .font(.title2)
+                    .padding()
+                Text("Здесь появится купленная Вами в \niTunes Store музыка")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+            }
+            .navigationTitle("Медиатека")
+        }
+    }
+}
+
+struct MiniPlayerView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "")
+                .frame(width: 50, height: 50)
+            Text("Не исполняется")
+            Button(action: {
+                //
+            }, label: {
+                Image(systemName: "play.fill")
+                    .foregroundColor(.black)
+            })
+        }
     }
 }
 
