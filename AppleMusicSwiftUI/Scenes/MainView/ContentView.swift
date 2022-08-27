ed7077d6ed56ed20b0cc35ed3f544c2e016decd2
KorobskoyRoman <shopone023@gmail.com>
2022-08-28 02:32:34 +0300

    Moved App and Scenes

---
+++ AppleMusicSwiftUI/Scenes/MainView/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Roman Korobskoy on 26.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabViewIcons()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
