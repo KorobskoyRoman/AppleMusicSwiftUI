970ddc408c32841bc87e08c2567a32665f7ba29a
KorobskoyRoman <shopone023@gmail.com>
2022-09-01 20:55:07 +0300

    Configured radio view

--- AppleMusicSwiftUI/Scenes/Radio/RadioView.swift
+++ AppleMusicSwiftUI/Scenes/Radio/RadioView.swift
@@ -8,39 +8,14 @@
 import SwiftUI
 
 struct RadioView: View {
-    @State private var genres = CreateRadioModel.createModel()
-    private let contentRows = [GridItem(.fixed(200))]
-
     var body: some View {
         ScrollView(.vertical) {
-            ScrollView(.horizontal, showsIndicators: false) {
-                LazyHGrid(rows: contentRows) {
-                    ForEach(0..<genres.count, id: \.self) { genre in
-                        VStack {
-                            Divider()
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text(genres[genre].title)
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
-                                Text(genres[genre].name)
-                                    .font(.title2)
-                                Text(genres[genre].description ?? "")
-                                    .foregroundColor(.secondary)
-                                Image(genres[genre].image)
-                                    .resizable()
-                                    .cornerRadius(10)
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(height: 250)
-                            }
-                            .padding([.leading])
-                        }
-                    }
-                }
-            }
-            ScrollView(.vertical) {
+            GenresView()
 
-            }
+            StationView()
+                .padding()
         }
+        .padding(.bottom)
     }
 }
 
